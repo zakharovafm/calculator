0a01642987d9e6ee8f6b7576ccbca2e617ec6b33
Anya Zakharova <annushka.zakharova28@gmail.com>
2021-11-16 19:05:57 +0300

    fixes with model

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,34 +1,6 @@
-import javax.management.InvalidAttributeValueException
 
 class Calculator {
-    private val operators = arrayOf("+", "-", "*", "/")
-
-    fun calculate(value:String):String {
-        var operator = ""
-        var operatorIndex:Int = -1
-        val trimValue = value.replace("\\s".toRegex(), "")
-
-        run getOperator@ {
-            operators.forEach {
-                operatorIndex = trimValue.indexOf(it)
-
-                if (operatorIndex != -1){
-                    operator = trimValue[operatorIndex].toString()
-                    return@getOperator
-                }
-            }
-        }
-
-        if(operatorIndex < 0){
-            throw InvalidAttributeValueException("Ошибка, нет оператора")
-        }
-
-        val a:Double = trimValue.slice(0 until operatorIndex).toDouble()
-        val b:Double = trimValue.substring(operatorIndex + 1).toDouble()
-        return operation(operator, a, b).toString()
-    }
-
-    private fun operation(operator:String, a:Double, b:Double):Double {
+    fun operation(operator:String, a:Double, b:Double):Double {
         when (operator) {
             "+" -> return a + b
             "*" -> return a * b
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,7 +1,134 @@
+package com.example.calculator
+
+import Calculator
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 
-fun main (args: Array<String>){
-    val calculator = Calculator()
-    val operation:String = readLine().toString()
-    println(calculator.calculate(operation))
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button0:Button? = findViewById(R.id.button0)
+        val button1:Button? = findViewById(R.id.button1)
+        val button2:Button? = findViewById(R.id.button2)
+        val button3:Button? = findViewById(R.id.button3)
+        val button4:Button? = findViewById(R.id.button4)
+        val button5:Button? = findViewById(R.id.button5)
+        val button6:Button? = findViewById(R.id.button6)
+        val button7:Button? = findViewById(R.id.button7)
+        val button8:Button? = findViewById(R.id.button8)
+        val button9:Button? = findViewById(R.id.button9)
+        val buttonAdd:Button? = findViewById(R.id.buttonAdd)
+        val buttonSub:Button? = findViewById(R.id.buttonSub)
+        val buttonDiv:Button? = findViewById(R.id.buttonDiv)
+        val buttonMul:Button? = findViewById(R.id.buttonMul)
+        val buttonDot:Button? = findViewById(R.id.buttonDot)
+        val buttonC:Button? = findViewById(R.id.buttonC)
+        val buttonEqual:Button? = findViewById(R.id.buttonEqual)
+        val textField:EditText? = findViewById(R.id.textField)
+
+        var prevNum = 0f
+        var currentNum = 0f
+
+        var lastAction = ""
+
+        button1!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "1") }
+
+        button2!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "2") }
+
+        button3!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "3") }
+
+        button4!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "4") }
+
+        button5!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "5") }
+
+        button6!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "6") }
+
+        button7!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "7") }
+
+        button8!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "8") }
+
+        button9!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + "9") }
+
+        button0!!.setOnClickListener { textField!!.setText(textField.text.toString() + "0") }
+
+        buttonAdd!!.setOnClickListener {
+            if (textField?.text.toString() == "") {
+                textField?.setText("")
+            } else {
+                val number = (textField?.text.toString()).toFloat()
+                if (prevNum == 0f){
+                    prevNum = number
+                }
+                else{
+                    prevNum += number
+                }
+
+                lastAction = "+"
+                textField?.setText("")
+            }
+        }
+
+        buttonSub!!.setOnClickListener {
+            if (textField?.text.toString() == "") {
+                textField?.setText("")
+            } else {
+                val number = (textField?.text.toString()).toFloat()
+                if (prevNum == 0f){
+                    prevNum = number
+                }
+                else{
+                    prevNum -= number
+                }
+                lastAction = "-"
+                textField?.setText("")
+            }
+        }
+
+        buttonMul!!.setOnClickListener {
+            if (textField?.text.toString() == "") {
+                textField?.setText("")
+            } else {
+                val number = (textField?.text.toString()).toFloat()
+                if (prevNum == 0f){
+                    prevNum = number
+                }
+                else{
+                    prevNum *= number
+                }
+                lastAction = "*"
+                textField?.setText("")
+            }
+        }
+
+        buttonDiv!!.setOnClickListener {
+            if (textField?.text.toString() == "") {
+                textField?.setText("")
+            } else {
+                val number = (textField?.text.toString()).toFloat()
+                if (prevNum == 0f){
+                    prevNum = number
+                }
+                else{
+                    prevNum /= number
+                }
+                lastAction = "/"
+                textField?.setText("")
+            }
+        }
+
+        buttonEqual!!.setOnClickListener {
+            currentNum = textField!!.text.toString().toFloat()
+            val calculator = Calculator()
+            textField.setText(calculator.operation(lastAction, prevNum.toDouble(), currentNum.toDouble()).toString())
+        }
+
+        buttonC!!.setOnClickListener { textField!!.setText("") }
+
+        buttonDot!!.setOnClickListener { textField!!.setText(textField!!.text.toString() + ".") }
+    }
 }
